59db71c565c15c93e523bc89dc050d0e9f03b9cb
Tiago Teixeira Santos <tiagots86@hotmail.com>
2024-12-08 12:29:20 -0300

    comunicação com fila sqs

---
+++ deployment/environment/sa-east-1/terraform.tfvars
@@ -0,0 +1,13 @@
+project_name = "sales-workload"
+
+region = "sa-east-1"
+
+sqs_processing_sales_config = {
+  queue_name                    = "sales-processing"
+  delay_seconds                 = 0
+  max_message_size              = 262144
+  message_retention_seconds     = 86400
+  receive_wait_time_seconds     = 10
+  visibility_timeout_seconds    = 60
+  dlq_redrive_max_receive_count = 4
+}
---
+++ deployment/variables.tf
@@ -0,0 +1,20 @@
+variable "project_name" {
+
+}
+
+variable "region" {
+
+}
+
+variable "sqs_processing_sales_config" {
+  type = object({
+    queue_name                    = string
+    delay_seconds                 = number
+    max_message_size              = number
+    message_retention_seconds     = number
+    receive_wait_time_seconds     = number
+    visibility_timeout_seconds    = number
+    dlq_redrive_max_receive_count = number
+  })
+
+}
