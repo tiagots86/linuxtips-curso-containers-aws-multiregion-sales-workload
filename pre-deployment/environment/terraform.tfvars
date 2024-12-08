e8c462ff0d3c58ef3505c8899869ff96b9dc9e70
Tiago Teixeira Santos <tiagots86@hotmail.com>
2024-12-08 09:46:53 -0300

    dynamodb

--- pre-deployment/environment/terraform.tfvars
+++ pre-deployment/environment/terraform.tfvars
@@ -6,3 +6,18 @@ active_states = {
   us-east-1 = "ACTIVE"
   sa-east-1 = "PASSIVE"
 }
+
+dynamodb_idempotency = {
+  name         = "sales-workload-idempotency"
+  billing_mode = "PROVISIONED"
+
+  point_in_time_recovery = true
+
+  read_min                 = 10
+  read_max                 = 100
+  read_autoscale_threshold = 80
+
+  write_min                 = 10
+  write_max                 = 100
+  write_autoscale_threshold = 80
+}
--- pre-deployment/variables.tf
+++ pre-deployment/variables.tf
@@ -19,3 +19,20 @@ variable "active_states" {
 
 }
 
+variable "dynamodb_idempotency" {
+  type = object({
+    name                   = string
+    billing_mode           = string
+    point_in_time_recovery = bool
+
+    read_min                 = number
+    read_max                 = number
+    read_autoscale_threshold = number
+
+    write_min                 = number
+    write_max                 = number
+    write_autoscale_threshold = number
+
+  })
+}
+
